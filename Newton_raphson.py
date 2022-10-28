2810e0af372928a7c6bc6a0c1455b193d6bb647c
Ayushj20 <112054518+Ayushj20@users.noreply.github.com>
2022-10-28 20:50:31 +0530

    Update Newton_raphson.py

--- Newton_raphson.py
+++ Newton_raphson.py
@@ -5,8 +5,7 @@ Assumptions:
 def f(x):
   return ((x*x)-x-1)
 def f1(x1):
-  return (2*x-1) 
-a=float(input("Enter \"a\" value\n"))
+  return (2*x1-1) 
 b=float(input("Enter \"b\" value\n"))
 e=float(input("Enter error\n"))
 if(f(b)==0):
@@ -16,11 +15,11 @@ else:
   i=1
 try :
    while(f(abs(f(m)))<e):
-     print(i,a,b,m,f(m))
+     print(i,b,m,f(m))
      b=m
      m=b-(f(b)/f1(b))
      i=i+1
-   print(i,a,b,m,f(m))
+   print(i,b,m,f(m))
    print("Root are",m)
 except:
   print("can\'t finding root" )
